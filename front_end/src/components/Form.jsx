456d070935d1602e2c7e40c3e47c5971ddc78869
thomas-spinec <thomas.spinec@laplateforme.io>
2023-11-28 16:16:38 +0100

    Add form validation and submit handler

--- front_end/src/components/Form.jsx
+++ front_end/src/components/Form.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { useContext, useState, useEffect } from "react";
 
 export default function Form({ options, type }) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
   return (
     <form>
       <label>Catégorie</label>
@@ -17,14 +22,17 @@ export default function Form({ options, type }) {
           : null}
       </select>
       <label>Montant</label>
-      <input type="text" />
+      <input type="text" required />
       <label>Titre</label>
-      <input type="text" />
+      <input type="text" required />
       <label>Date</label>
-      <input type="date" />
+      <input
+        type="date"
+        max={new Date().toISOString().split("T")[0]}
+        required
+      />
       <input type="hidden" value={type} />
-      <input type="submit" />
+      <input type="submit" onClick={handleSubmit} />
     </form>
   );
 }
-
